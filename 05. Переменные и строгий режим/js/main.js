07f8f6568824790131bb20aa61a280a39f89f7f9
K1mskii <movies2702@gmail.com>
2021-05-24 12:07:08 +0600

    Переменные и строгий режим

---
+++ 05. Переменные и строгий режим/js/main.js
@@ -0,0 +1,25 @@
+'use strict'; // Современный режим - строгий режим
+
+
+// Переменные let, var, const
+
+const leftBorderWidth = 1; // camel case
+
+// Let
+let number = 5;
+number = 10;
+console.log(number); // 10
+
+
+// Const
+const obj = {
+    a: 50
+};
+obj.a = 10;
+console.log(obj); // {a: 10}
+
+// Var - видна везде, лучше не использовать
+console.log(name); // undefined
+var name = 'Oleg';
+
+
