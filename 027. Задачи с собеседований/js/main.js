4c9c4c7e75a354e6627c51508f6a9e141a06c13e
K1mskii <movies2702@gmail.com>
2021-06-02 11:13:21 +0600

    Задачи с собеседований

---
+++ 027. Задачи с собеседований/js/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// let x = 5; alert( x++ ); // 5
+
+// console.log([] + false - null + true); // false, NaN, NaN
+
+// let y = 1; 
+// let x = y = 2;
+// alert(x); // 2
+
+// console.log([] + 1 + 2); // string: 12
+
+// alert("1"[0]); // 1
+
+// console.log(2 && 1 && null && 0 && undefined); // null
+// и запинается на лжи
+// или запинается на правде
+
+// console.log(!!(1 && 2) === (1 && 2)); // false
+
+// alert(null || 2 && 3 || 4); // 3
+
+// const a = [1, 2, 3];
+// const b = [1, 2, 3];
+// console.log(a == b); // false
+
+// alert( +'Infinity'); // number: Infinity
+
+// console.log("Ежик" > "Яблоко"); // false
+
+// console.log(0 || "" || 2 || undefined || true || false); // 2
