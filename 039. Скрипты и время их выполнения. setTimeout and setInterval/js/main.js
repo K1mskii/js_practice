70c8afa5ce003b7e3884c31121943b7d51818664
K1mskii <movies2702@gmail.com>
2021-06-13 11:24:42 +0600

    Работа с датами

--- 039. Скрипты и время их выполнения. setTimeout and setInterval/js/main.js
+++ 039. Скрипты и время их выполнения. setTimeout and setInterval/js/main.js
@@ -30,14 +30,13 @@ function myAnimation() {
     let pos = 0;
 
     const id = setInterval(frame, 10);
-
     function frame() {
         if (pos == 300) {
             clearInterval(id);
         } else {
             pos++;
             elem.style.top = pos + "px";
-            elem.style.left = pos + 'px';
+            elem.style.left = pos + "px";
         }
     }
 }
